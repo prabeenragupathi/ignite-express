070ee61286adea1d5910656568ad88e0d2ce8f0e
Prabeen <74868630+prabeenragupathi@users.noreply.github.com>
2025-04-29 11:17:42 +0530

    Update index.ts

--- templates/ts/index.ts
+++ templates/ts/index.ts
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from Ignite Express!");
+  res.send("Hello from Quick Express Gen!");
 });
 
 app.listen(PORT, () => {
